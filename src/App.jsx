4aa48799afba48d1b075613882a60a10349afadb
Adam H <ad.hauck@gmail.com>
2025-09-15 16:04:39 -0700

    Set up basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/about" element={<h1>About Page</h1>} />
+          <Route path="/results" element={<h1>Results Page</h1>} />
+          <Route path="/contact" element={<h1>Contact Page</h1>} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
