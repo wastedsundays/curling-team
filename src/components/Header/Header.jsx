e7c19403943ce658cf2362af6ad0da11a63d1725
Adam H <ad.hauck@gmail.com>
2025-09-16 14:48:29 -0700

    Added proper logo/icon to header/nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import Navigation from './Navigation';
+import Logo from '../../assets/slide-family-stone-logo.svg';
 
 
 const Header = () => {
@@ -13,7 +14,7 @@ const Header = () => {
 
     return (
         <header>
-            <img src='https://picsum.photos/100/70' alt='placeholder logo' onClick={toggleNav} id='logo' className='logo'/>
+            <img src={Logo} alt='Slide & The Family Stone Logo' onClick={toggleNav} id='logo' className='logo'/>
             <Navigation isNavOpen={isNavOpen} toggleNav={toggleNav} />
         </header>
     );
