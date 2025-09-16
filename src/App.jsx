eaf85debedabd59c2ff314fb7c09da20a675a79e
Adam H <ad.hauck@gmail.com>
2025-09-16 13:10:27 -0700

    Added About Page to router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<h1>About Page</h1>} />
+          <Route path="/about" element={<AboutPage />} />
           <Route path="/results" element={<h1>Results Page</h1>} />
           <Route path="/contact" element={<h1>Contact Page</h1>} />
           <Route path="*" element={<h1>404 Not Found</h1>} />
