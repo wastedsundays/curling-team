e473f608a41c72c04a9d6c5bc9c6dd20117d30dd
Adam H <ad.hauck@gmail.com>
2025-09-16 23:15:48 -0700

    footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
-
+import '../styles/footer-styles.css';
 
 const Footer = () => {
     return (
-        <footer>
-            <div>
-                <p>&copy; {new Date().getFullYear()} Slide & the Family Stone</p>
-                <p>Website by Adam H</p>
-                <p>Proudly curling at Marpole Curling Club.</p>
+        <footer className='dark-bg-transparent'>
+            <div className='footer-details'>
+                <p className='footer-copy'>&copy; {new Date().getFullYear()} Slide & the Family Stone</p>
+                <p className='footer-website'>Website by Adam H</p>
+                <p className='footer-club'>Proudly curling at Marpole Curling Club.</p>
             </div>
-            <div>
+            <div className='footer-joke'>
                 <p>A clever curling joke goes here.</p>
             </div>
         </footer>
