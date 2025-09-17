f071cdee708d60a4ca5f8d497c7bc87a2b03e8eb
Adam H <ad.hauck@gmail.com>
2025-09-16 22:28:07 -0700

    Added flowers sunday font, set up global styles css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './App.css'
+import './styles/global-styles.css';
 
 import Header from './components/Header/Header'
 import HomePage from './pages/HomePage'
