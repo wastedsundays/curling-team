a83e2a1b42379e1be4ef66f81f40113b115d57f1
Adam H <ad.hauck@gmail.com>
2025-09-18 12:25:16 -0700

    Added random curling joke in footer (with json)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+
 import '../styles/footer-styles.css';
 
 const Footer = () => {
+
+    const [quote, setQuote] = useState(null);
+    const location = useLocation();
+
+    useEffect(() => {
+        const fetchQuote = async () => {
+            try {
+                const response = await fetch('/data/footer-jokes.json');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                const randomIndex = Math.floor(Math.random() * data.jokes.length);
+                setQuote(data.jokes[randomIndex]);
+            } catch (error) {
+                console.error('Error fetching quote:', error);
+            }
+
+        }
+        fetchQuote();
+    }, [location]);
+
     return (
         <footer className='dark-bg-transparent'>
             <div className='footer-details'>
@@ -9,7 +35,7 @@ const Footer = () => {
                 <p className='footer-club'>Proudly curling at Marpole Curling Club.</p>
             </div>
             <div className='footer-joke'>
-                <p>A clever curling joke goes here.</p>
+                <p>{quote}</p>
             </div>
         </footer>
     )
