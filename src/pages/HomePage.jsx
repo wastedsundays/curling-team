f1f2897074fd2a8d94f3c46d856e527f097f167f
Adam H <ad.hauck@gmail.com>
2025-09-18 11:13:10 -0700

    set up styles for split column layouts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,15 +17,18 @@ const HomePage = () => {
         <section className='section-spacing'>
             <StatGrid />
         </section>
-        <section className='section-spacing dark-bg-transparent'>
-            <h2>What We're All About</h2>
+        <section className='section-spacing dark-bg-transparent about-section'>
+        <div className='split-columns'>
+            <h2 className='hide-50rem'>What We're All About</h2>
             <div>
+                <h2 className='show-50rem'>What We're All About</h2>
                 <p>We're 4 guys who were all in the same Thursday league and thought it would be fun to take on the Monday Open League.</p>
                 <p>Sure, our technique is questionable, our strategy meetings are debating whose turn it is to buy drinks and 50/50 tickets. But what we lack in skill, we make up for in determination to have the best possible time on the ice.</p>
-                <p>We curl for the love of the game, the frindship, and the inexplicable joy of yelling at a 44 pound slab of granite.</p>
+                <p>We curl for the love of the game, the friendship, and the inexplicable joy of yelling at a 44 pound slab of granite.</p>
 
             </div>
             <img src='https://picsum.photos/600' alt='placeholder image' />
+        </div>
         </section>
         <section className='section-spacing'>
             <h2 className='center-text-700'>Everything You Need to Know</h2>
