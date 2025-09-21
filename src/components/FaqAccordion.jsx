fa8e847b7077143f6a5a94100b8b0bb63223f79f
Adam H <ad.hauck@gmail.com>
2025-09-21 01:25:59 -0700

    added accordion functionality and styling

--- src/components/FaqAccordion.jsx
+++ src/components/FaqAccordion.jsx
@@ -1,19 +1,29 @@
-
+import * as Accordion from '@radix-ui/react-accordion';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
+import '../styles/faq-styles.css';
 
 const FaqAccordion = ({ data }) => {
-    return (
-        <div>
-            <h2 className='center-text'>Questions?</h2>
-                <ul>
-                    {data.map((item, index) => (
-                        <li key={index}>
-                            <strong>{item.question}</strong>
+return (
+        <div className="faq-container">
+            <h2 className="center-text">Questions?</h2>
+            <Accordion.Root type="single" collapsible className="accordion-root">
+                {data.map((item, index) => (
+                    <Accordion.Item value={`item-${index}`} key={index} className="accordion-item">
+                        <Accordion.Header className="accordion-header">
+                            <Accordion.Trigger className="accordion-trigger">
+                                {item.question}
+                                <ChevronDownIcon className="arrow" />
+                            </Accordion.Trigger>
+                        </Accordion.Header>
+                        <Accordion.Content className="accordion-content">
                             <p>{item.answer}</p>
-                        </li>
-                    ))}
-                </ul>
+                        </Accordion.Content>
+                    </Accordion.Item>
+                ))}
+            </Accordion.Root>
         </div>
     );
 };
 
 export default FaqAccordion;
+
