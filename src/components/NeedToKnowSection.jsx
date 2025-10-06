be5fd3d92d36fd14b4bc5bca44fb14b54a4b5418
Adam H <ad.hauck@gmail.com>
2025-10-06 15:03:14 -0700

    Created home/about component and updated hero image sizing on desktop

--- src/components/NeedToKnowSection.jsx
+++ src/components/NeedToKnowSection.jsx
@@ -2,17 +2,14 @@ import { Link } from 'react-router-dom';
 //eslint-disable-next-line no-unused-vars
 import { motion } from 'framer-motion';
 import { fadeInUp, 
+
          viewportSettingsHalf,
 
         } from '../utilities/AnimationVariants';
 
 const NeedToKnowSection = () => {
 
-    const getVariantByIndex = (index) => {
-        const mobileVariant = mobileOnly.fadeIn;
-        const desktopVariant = fadeInUp;
-        return conditionalAnimation(mobileVariant, desktopVariant);
-    };
+
     return (
        <section className='section-spacing'>
             <motion.h2 
@@ -26,30 +23,37 @@ const NeedToKnowSection = () => {
                     Everything You Need to Know
             </motion.h2>
             <div className='need-to-know-grid'>
-                <Link to = '/about'>
+                
+                    <Link to = '/about'>
+                        <article className='need-to-know-article'>
+                            <img src='https://adamh.ca/slidefamilycurling/images/meet-the-team-image.webp' alt='A comic book style illustration of the curling team in superhero uniforms' />
+                            <h3>Meet the Team</h3>
+                            <p>Get to know the people responsible for this beautiful disaster.</p>
+                            <p className='red-text'>Learn About Us {'>'}</p>
+                        </article>
+                    </Link>
+
+
+
+                    <Link to = '/results'>
                     <article className='need-to-know-article'>
-                        <img src='https://adamh.ca/slidefamilycurling/images/meet-the-team-image.webp' alt='A comic book style illustration of the curling team in superhero uniforms' />
-                        <h3>Meet the Team</h3>
-                        <p>Get to know the people responsible for this beautiful disaster.</p>
-                        <p className='red-text'>Learn About Us {'>'}</p>
+                        <img src='https://adamh.ca/slidefamilycurling/images/results-link-image.webp' alt='An empty curling club with sheets and a scoreboard' />
+                        <h3>Results</h3>
+                        <p>Check out our schedule and see how we're doing this season.</p>
+                        <p className='red-text'>See Our Results {'>'}</p>
                     </article>
-                </Link>
-                <Link to = '/results'>
-                <article className='need-to-know-article'>
-                    <img src='https://adamh.ca/slidefamilycurling/images/results-link-image.webp' alt='An empty curling club with sheets and a scoreboard' />
-                    <h3>Results</h3>
-                    <p>Check out our schedule and see how we're doing this season.</p>
-                    <p className='red-text'>See Our Results {'>'}</p>
-                </article>
-                </Link>
-                <Link to = '/contact'>
-                    <article className='need-to-know-article'>
-                        <img src='https://adamh.ca/slidefamilycurling/images/get-in-touch-image.webp' alt='A curler yelling at his sweepers' />
-                        <h3>Get in Touch</h3>
-                        <p>Questions about our technique? Want to challenge us? Good communication is important in curling.</p>
-                        <p className='red-text'>Contact Us {'>'}</p>
-                    </article>
-                </Link>
+                    </Link>
+
+
+                    <Link to = '/contact'>
+                        <article className='need-to-know-article'>
+                            <img src='https://adamh.ca/slidefamilycurling/images/get-in-touch-image.webp' alt='A curler yelling at his sweepers' />
+                            <h3>Get in Touch</h3>
+                            <p>Questions about our technique? Want to challenge us? Good communication is important in curling.</p>
+                            <p className='red-text'>Contact Us {'>'}</p>
+                        </article>
+                    </Link>
+
             </div>
         </section>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,6 +10,7 @@ import { fadeInUp,
         } from '../utilities/AnimationVariants';
 import LinkButton from '../components/LinkButton';
 import StatGrid from '../components/StatGrid';
+import HomeAboutSection from '../components/HomeAboutSection';
 import NeedToKnowSection from '../components/NeedToKnowSection';
 import Testimonials from '../components/Testimonials';
 import '../styles/home-styles.css';
@@ -38,19 +39,8 @@ const HomePage = () => {
         <section className='section-spacing'>
             <StatGrid />
         </section>
-        <section className='section-spacing dark-bg-transparent about-section'>
-        <div className='split-columns'>
-            <h2 className='hide-50rem'>What We're All About</h2>
-            <div>
-                <h2 className='show-50rem'>What We're All About</h2>
-                <p>We're 4 guys who were all in the same Thursday league and thought it would be fun to take on the Monday Open League.</p>
-                <p>Sure, our technique is questionable, our strategy meetings are debating whose turn it is to buy drinks and 50/50 tickets. But what we lack in skill, we make up for in determination to have the best possible time on the ice.</p>
-                <p>We curl for the love of the game, the friendship, and the inexplicable joy of yelling at a 44 pound slab of granite.</p>
 
-            </div>
-            <img src='https://adamh.ca/slidefamilycurling/images/what-were-about.webp' alt='A curling team sweeps a rock while the skip shouts encouragement' />
-        </div>
-        </section>
+        <HomeAboutSection />
 
         <NeedToKnowSection />
 
