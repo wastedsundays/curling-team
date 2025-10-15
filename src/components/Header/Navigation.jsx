f8dfb137385085d77308359b815e4b89cc19e938
Adam H <ad.hauck@gmail.com>
2025-10-15 11:04:37 -0700

    Added active page link styling to header nav.

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -1,6 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const Navigation = ({ isNavOpen, toggleNav }) => {
+
+    const location = useLocation();
+    const [activeTab, setActiveTab] = useState(0);
+
+    useEffect(() => {
+        switch (true) {
+            case location.pathname === '/':
+                setActiveTab(0);
+                break;
+            case location.pathname === '/about':
+                setActiveTab(1);
+                break;
+            case location.pathname === '/results':
+                setActiveTab(2);
+                break;
+            case location.pathname === '/contact':
+                setActiveTab(3);
+                break;
+            default:
+                setActiveTab(null);
+                break;
+        }
+    }, [location]);
 
     const handleLinkClick = () => {
         toggleNav();
@@ -9,10 +33,10 @@ const Navigation = ({ isNavOpen, toggleNav }) => {
     return (
         <nav className={isNavOpen ? 'open' : ''}>
             <ul className='nav-links'>
-                <li><Link to='/' onClick={handleLinkClick}>Home</Link></li>
-                <li><Link to='/about' onClick={handleLinkClick}>About</Link></li>
-                <li><Link to='/results' onClick={handleLinkClick}>Results</Link></li>
-                <li><Link to='/contact' onClick={handleLinkClick}>Contact</Link></li>
+                <li className={activeTab === 0 ? 'active-header-link' : 'header-link'}><Link to='/' onClick={handleLinkClick}>Home</Link></li>
+                <li className={activeTab === 1 ? 'active-header-link' : 'header-link'}><Link to='/about' onClick={handleLinkClick}>About</Link></li>
+                <li className={activeTab === 2 ? 'active-header-link' : 'header-link'}><Link to='/results' onClick={handleLinkClick}>Results</Link></li>
+                <li className={activeTab === 3 ? 'active-header-link' : 'header-link'}><Link to='/contact' onClick={handleLinkClick}>Contact</Link></li>
             </ul>
         </nav>
     );
