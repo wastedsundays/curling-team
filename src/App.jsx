e9bb2217c0d169e480d6e8aa44e5d8d45fcc5dde
Adam H <ad.hauck@gmail.com>
2026-09-18 14:30:00 -0700

    Added fancystats route to app routing

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/results" element={<ResultsPage />} />
+          <Route path="/fancy-stats" element={<ResultsPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="*" element={<h1>404 Not Found</h1>} />
         </Routes>
